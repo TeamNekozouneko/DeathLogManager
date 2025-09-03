0e6b244d6b614785d8ed3f054bb8ae34fc5fd91f
るんく君 <91005690+RunqRun@users.noreply.github.com>
2025-09-03 16:48:42 +0900

    init

---
+++ src/main/kotlin/com/nekozouneko/deathLogManager/DeathLogManager.kt
@@ -0,0 +1,14 @@
+package com.nekozouneko.deathLogManager
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class DeathLogManager : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
